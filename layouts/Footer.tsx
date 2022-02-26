95e4bc36c23160ce209e6f83e163a9924fb74275
Kunal Bagaria <kb24x7@gmail.com>
2022-02-26 18:09:25 +0530

    added: footer content

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -5,15 +5,25 @@ import nebIcon from '@/images/icons/neb.svg';
 export const Footer = () => (
   <div className={styles.container}>
     <h1 className={styles.heading}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt sit in posuere ligula etiam vestibulum tincidunt nisi.
+      Join us and help us create the revolution in the third web.
     </h1>
     <div className={styles.btnFlex}>
-      <button className={`${styles.btn} ${styles.blackBtn}`}>
-        Check this CTA
+      <button
+        onClick={() => {
+          window.open('https://lu.ma/superteamDAO', '_blank');
+        }}
+        className={`${styles.btn} ${styles.blackBtn}`}
+      >
+        Apply for Membership
         <img src={newIcon.src} alt="" />
       </button>
-      <button className={`${styles.btn} ${styles.whiteBtn}`}>
-        Check this CTA
+      <button
+        onClick={() => {
+          window.open('https://discord.gg/6jQkrFayfH', '_blank');
+        }}
+        className={`${styles.btn} ${styles.whiteBtn}`}
+      >
+        Join our Discord
         <img src={nebIcon.src} alt="" />
       </button>
     </div>
