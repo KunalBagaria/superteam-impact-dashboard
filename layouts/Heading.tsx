cd41109b9943710ccbeff7cc55c113961fd5e15d
Kunal Bagaria <61944452+KunalBagaria@users.noreply.github.com>
2022-04-09 20:34:24 +0530

    update

--- layouts/Heading.tsx
+++ layouts/Heading.tsx
@@ -3,7 +3,7 @@ import styles from "@/styles/Home.module.scss";
 export const MainHeading = () => (
   <h1 className={styles.mainHeading}>
     <span style={{ fontWeight: 500, marginRight: "1.5rem" }}>
-      SuperTeam Impact
+      Superteam Impact
     </span>
     Dashboard
   </h1>
