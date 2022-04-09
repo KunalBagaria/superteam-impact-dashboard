b6d0c15b5073ced7e2f072181575e375ce49f5db
imukerji <ishikamukerji@gmail.com>
2022-04-08 20:08:24 -0500

    updated copyrighting

--- layouts/Heading.tsx
+++ layouts/Heading.tsx
@@ -1,11 +1,9 @@
-import styles from '@/styles/Home.module.scss';
+import styles from "@/styles/Home.module.scss";
 
 export const MainHeading = () => (
   <h1 className={styles.mainHeading}>
-    <span
-      style={{ fontWeight: 500, marginRight: '1.5rem' }}
-    >
-      Impact
+    <span style={{ fontWeight: 500, marginRight: "1.5rem" }}>
+      SuperTeam Impact
     </span>
     Dashboard
   </h1>
